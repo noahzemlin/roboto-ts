6057d3eb9d460815161017d1f0e9a5b1bbddab4c
Justin Wilford <jwilford@usc.edu>
2019-08-05 16:23:49 -0700

    Styled 404 page better; Made HTML in app.js legible

--- server/app.js
+++ server/app.js
@@ -10,20 +10,20 @@ const port = 8080;
 const controller = require('./controller');
 
 const errorResponse = (`
-<h1>Page not found!</h1><br />
-You requested in the wrong neighborhood <br />
-<iframe src='https://gfycat.com/ifr/FluidSparklingIndianrockpython' frameborder='0' scrolling='no' allowfullscreen width='640' height='412'></iframe>
-<p> <a href="https://gfycat.com/fluidsparklingindianrockpython">via Gfycat</a></p>
+<body style="font-family: arial">
+    <h1>Page not found!</h1>
+    You requested in the wrong neighborhood <br />
+    <iframe src='https://gfycat.com/ifr/FluidSparklingIndianrockpython' frameborder='0' scrolling='no' allowfullscreen width='640' height='412'></iframe>
+    <p> <a href="https://gfycat.com/fluidsparklingindianrockpython">via Gfycat</a></p>
+</body>
 `);
 
-//Define API Routes here
-//Routes are resolved in order, from top to bottom. If a request is matched to multiple routes, it will be send to only the first match (unless that match is middleware)
+//Define API Routes/endpoints here
+//Routes are resolved in order, from top to bottom. If a request is matched to multiple routes, it will be sent to just the first match (unless that match is middleware)
 
 app.get('/msg', controller.sendTestMsg);
 
 app.get('*', (req, res) => res.status(404).send(errorResponse));
-
-https://gfycat.com/fluidsparklingindianrockpython
 
 
 app.listen(port, () => console.log(`Express app started and listening on port ${port}.`));
